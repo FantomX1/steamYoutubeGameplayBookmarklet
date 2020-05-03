38006b5a2d5e41348c07b14838afc5ef9af5b9ae
frantisek.hallo <frantisek.hallo@ferratum.com>
2020-05-04 00:24:39 +0200

    js bookmarklet for seeking automatically youtube gameplay results to the current steam store game for prompt seeing the gameplay of the game

---
+++ boomkarklet.js
@@ -0,0 +1,1 @@
+javascript:var a="https://www.youtube.com/results?search_query=",b=jQuery('.apphub_AppName').text(); var url =a+b+' gameplay'; var win = window.open(url, '_blank'); win.focus(); void(0);
